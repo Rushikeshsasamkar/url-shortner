acdf7eb439b4f4c6eb2712d66a3e667983de3009
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-12-23 15:55:19 +0530

    latest commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const createHttpError = require('http-errors')
 const mongoose = require('mongoose')
 const path = require('path')
 const ShortUrl = require('./models/url.model')
-const env = require('dotenv').config('./env')
+const env = require('dotenv').config()
 
 const app = express()
 app.use(express.static(path.join(__dirname, 'public')))
